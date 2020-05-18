3f3d2325876d77d8d6131151908fda94e1025b10
Jack Ellis <jackellis1504@gmail.com>
2020-05-18 09:51:43 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# RSS Bookmarking Tool
+
+### Structure
+
+There are 2 classes:
+
+- `User`
+- `Feed`
+
+A `User` can have many `Feed`s, and a `Feed` can belong to a single `User`.
+The `User` can create new `Feed`s.
---
+++ database/migrations/2020_05_15_165927_create_feeds_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFeedsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('feeds', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name');
+            $table->string('url');
+            $table->bigInteger('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('feeds');
+    }
+}
