787ce11b87929242d575e9ac43f8b13bc616464d
Jack Ellis <jackellis1504@gmail.com>
2020-05-18 15:05:37 +0100

    Added delete buttons

--- resources/views/feeds/index.blade.php
+++ resources/views/feeds/index.blade.php
@@ -9,12 +9,19 @@
     </div>
     @foreach($feeds as $feed)
         <div class="card mb-3">
-            <div class="card-header d-flex justify-content-between">
+            <div class="card-header d-flex justify-content-between align-items-center">
                 {{ $feed->name }}
                 @if($feed->user_id == Auth::user()->id)
-                <a href="{{ route('feeds.edit', compact('feed')) }}">
-                    Edit
-                </a>
+                <span class="d-flex flex-row">
+                    <a href="{{ route('feeds.edit', compact('feed')) }}" class="btn btn-primary mr-1">
+                        Edit
+                    </a>
+                    <form method="POST" action="{{ route('feeds.destroy', compact('feed'))}}">
+                        @method("DELETE")
+                        @csrf
+                        <input type="submit" value="Delete" class="btn btn-danger">
+                    </form>
+                </span>
                 @endif
             </div>
             <div class="card-body">
